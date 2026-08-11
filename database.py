512f70b7092643cb6c3e885a00c1789e9c0b7ec3
monszej <monszej@gmail.com>
2026-08-11 11:11:13 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,26 +1,53 @@
 import sqlite3
 
-def get_conn():
-    conn=sqlite3.connect('typer.db',check_same_thread=False)
-    conn.execute("CREATE TABLE IF NOT EXISTS users(username TEXT PRIMARY KEY,password TEXT)")
-    conn.execute("CREATE TABLE IF NOT EXISTS predictions(username TEXT,match_id INTEGER,home_pred INTEGER,away_pred INTEGER,PRIMARY KEY(username,match_id))")
-    return conn
-    def get_prediction(username, match_id):
 
-    cursor = get_conn().cursor()
+def get_conn():
+    conn = sqlite3.connect(
+        "typer.db",
+        check_same_thread=False
+    )
+
+    conn.execute(
+        """
+        CREATE TABLE IF NOT EXISTS users(
+            username TEXT PRIMARY KEY,
+            password TEXT
+        )
+        """
+    )
+
+    conn.execute(
+        """
+        CREATE TABLE IF NOT EXISTS predictions(
+            username TEXT,
+            match_id INTEGER,
+            home_pred INTEGER,
+            away_pred INTEGER,
+            PRIMARY KEY(username, match_id)
+        )
+        """
+    )
+
+    return conn
+
+
+def get_prediction(username, match_id):
+
+    conn = get_conn()
+    cursor = conn.cursor()
 
     cursor.execute(
         """
         SELECT home_pred, away_pred
         FROM predictions
-        WHERE username=? AND match_id=?
+        WHERE username = ?
+        AND match_id = ?
         """,
         (username, match_id)
     )
 
     result = cursor.fetchone()
 
-    if result:
-        return result
+    conn.close()
 
-    return None
+    return result
