826b4153e8e19744fcadbd0cedb2f17a78a5a2a6
monszej <monszej@gmail.com>
2026-08-11 14:29:27 +0200

    Update 1_Login.py

--- pages/1_Login.py
+++ pages/1_Login.py
@@ -1,25 +1,67 @@
 import streamlit as st
-from auth import create_users_table, add_user, verify_user
+from streamlit_cookies_manager import EncryptedCookieManager
+
+from auth import (
+    create_users_table,
+    add_user,
+    verify_user
+)
+
+# Cookies
+cookies = EncryptedCookieManager(
+    prefix="pl_typer_",
+    password="zmien_to_na_bardzo_dlugi_losowy_ciag"
+)
+
+if not cookies.ready():
+    st.stop()
 
 create_users_table()
 
-# pierwszy administrator
+# Administrator
 add_user("admin", "admin123", 1)
+
+# Automatyczne logowanie z cookie
+if cookies.get("username"):
+
+    st.session_state.logged_in = True
+    st.session_state.username = cookies["username"]
+
+    st.success(
+        f"✅ Automatycznie zalogowano jako {cookies['username']}"
+    )
+
+    st.stop()
 
 st.title("🔐 Logowanie")
 
 username = st.text_input("Użytkownik")
-password = st.text_input("Hasło", type="password")
+password = st.text_input(
+    "Hasło",
+    type="password"
+)
 
 if st.button("Zaloguj"):
 
-    if verify_user(username, password):
+    if verify_user(
+        username,
+        password
+    ):
 
         st.session_state.logged_in = True
         st.session_state.username = username
 
-        st.success(f"Witaj {username}")
+        cookies["username"] = username
+        cookies.save()
+
+        st.success(
+            f"Witaj {username}"
+        )
+
+        st.rerun()
 
     else:
 
-        st.error("Błędny login lub hasło")
+        st.error(
+            "Błędny login lub hasło"
+        )
