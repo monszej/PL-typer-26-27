65926907d147fc4bc828451f7a30ccd142a4f150
monszej <monszej@gmail.com>
2026-08-11 13:45:45 +0200

    Update 4_Typowanie.py

--- pages/4_Typowanie.py
+++ pages/4_Typowanie.py
@@ -1,5 +1,6 @@
-st.write(match["homeTeam"])
+
 import streamlit as st
+st.write(match["homeTeam"])
 from football_data import get_matches
 from database import get_conn, get_prediction
 from datetime import datetime, timezone
