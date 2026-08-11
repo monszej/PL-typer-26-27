d55458fcff259b49d6a1c4767c8b2889c0dd63d3
monszej <monszej@gmail.com>
2026-08-11 13:38:55 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Premier League Typer 26/27",
+    page_icon="⚽",
+    layout="wide"
+)
+
+st.title("⚽ Premier League Typer 26/27")
+
+st.markdown("""
+## Witaj w typerze Premier League
+
+Wybierz stronę z menu po lewej stronie.
+
+### Dostępne moduły:
+- Logowanie
+- Typowanie
+- Ranking
+- Moje typy
+- Statystyki
+- Panel administratora
+
+Wersja V2 w budowie 🚀
+""")
