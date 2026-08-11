e5f7e92008760436d5d1905edc5e48f3cceb5d33
monszej <monszej@gmail.com>
2026-08-11 10:44:28 +0200

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+def create_users_table():
+    conn = sqlite3.connect("typer.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        username TEXT PRIMARY KEY,
+        password TEXT,
+        is_admin INTEGER DEFAULT 0
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+
+def add_user(username, password, is_admin=0):
+    conn = sqlite3.connect("typer.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "INSERT OR REPLACE INTO users VALUES (?, ?, ?)",
+        (username, password, is_admin)
+    )
+
+    conn.commit()
+    conn.close()
+
+
+def verify_user(username, password):
+    conn = sqlite3.connect("typer.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT * FROM users WHERE username=? AND password=?",
+        (username, password)
+    )
+
+    user = cursor.fetchone()
+
+    conn.close()
+
+    return user is not None
